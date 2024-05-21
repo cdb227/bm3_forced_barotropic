b21a5c5df46a82a90a3d53ec548f54be95ae50ce
Cameron Bertossa <bertossa@rayleigh.aos.wisc.edu>
2024-05-21 15:40:35 -0500

    finished modularizing/reorganizing

--- src/model/solver.py
+++ src/model/solver.py
@@ -2,66 +2,59 @@ import numpy as np
 from tqdm import tqdm
 import xarray as xr
 
-#from forced_barotropic_sphere.dynamics import ForcedVorticity
-
-a = 6371e3         # Radius of the earth in m
-Omega = 7.29e-5    # Angular velocity of the earth in rad/s
-g00 = 9.81         # Acceleration due to gravity near the surface of the earth in m/s^2
-d2r = np.pi / 180. # Factor to convert degrees to radians
-r2d = 180. / np.pi # Factor to convert radians to degrees
-d2s = 86400      # Factor to convert from days to seconds
+import sys
+sys.path.append('../src')  # Add the 'src' directory to the Python path
+from model import forcing
+from utils import config, constants
 
 
 class Solver:
     """
     Solver class which will integrate the vorticity equation
     """
-    def __init__(self,  sphere, forcing, ofreq, **kwargs):
+    def __init__(self,  sphere, T, **kwargs):
         """
         initializes a solver for barotropic model.
         
         Arguments:
         * sphere (Sphere object) : contains spectral methods and grid for integration
-        * forcing (Forcing object) : contains timestep information and forcing to be applied to sphere
-        * ofreq (int) : return model output every ofreq-th step
+        * T (int)                : total integration time (seconds) 
         """
         
         self.sphere = sphere
+        self.T      = T
         
-        self.dt = forcing.dt                   #integration step
-        self.T = forcing.T                     #integration time
+        #get optional solver arguments
+        self.dt     = kwargs.get('dt', config.DEFAULT_dt) #integration timestep
+        self.ofreq  = kwargs.get('ofreq', config.DEFAULT_ofreq) #output freq
+        
+        #get optional dynamics arguments -- check utils/config for defaults.
+        self.temp_linear = kwargs.get('temp_linear', config.DEFAULT_temp_linear) #linear dynamics?
+        self.vort_linear = kwargs.get('vort_linear', config.DEFAULT_vort_linear)
+        self.tau         = kwargs.get('tau', config.DEFAULT_tau) * 1/constants.day2sec #thermal relaxation rate (per day)
+        self.rs          = kwargs.get('rs' , config.DEFAULT_rs)  * 1/constants.day2sec #frictional dissipation rate (per day)
+        self.r           = kwargs.get('robert_filter', config.DEFAULT_robert_filter)# Robert-Asselin filter strength
+        self.nu          = kwargs.get('nu', config.DEFAULT_nu )         # Hyperdiffusion coefficient
+        self.diffusion_order = kwargs.get('diffusion_order', config.DEFAULT_diffusion_order) # Hyperdiffusion order
+        
+        #get optional forcing arguments
+        self.forcing_type =kwargs.get('forcing_type', config.DEFAULT_forcing)
+        Si               = kwargs.get('red_eddy_start', None) #starting position for red-eddy
+        
+        
         self.Nt = int(self.T / self.dt)        #number of steps   
         self.ts = np.arange(self.Nt) * self.dt #integration times
         
-        self.No = int(self.Nt/ofreq)+1         #number of outputs
-        self.ofreq = ofreq                     #output frequency
-         
-        tau = kwargs.get('tau', 0)
-        Kappa = kwargs.get('Kappa', 0)
-        rs = kwargs.get('rs', 1/7.)
-        
-        self.tau = tau * 1/d2s      # Thermal relaxation rate
-        self.Kappa = Kappa*d2s    # 0.1 day thermal damping
-        self.rs = rs * 1/d2s      # frictional dissipation 7 days,
+        self.No = int(self.Nt/self.ofreq)+1         #number of outputs
 
-        # Hyperdiffusion coefficient and order
-        self.nu = kwargs.get('nu', 0.)
-        self.diffusion_order = kwargs.get('diffusion_order', 2)
-        
         #print('integrating with: ', 'nu=',self.nu,'diffusion_order=',self.diffusion_order)
-        
-        # Robert-Asselin filter strength
-        self.r = kwargs.get('robert_filter', 0.02)
-        
+               
         #use laplacian eigenvalues for damping/diffusion term
-        self.Vdamping = self.nu * np.abs(self.sphere._laplacian_eigenvalues) ** self.diffusion_order
-        
-        self.Tdamping = self.Kappa * np.abs(self.sphere._laplacian_eigenvalues) ** self.diffusion_order
-        
-        #TODO: integrate forcing
-        self.forcing = forcing.forcing_tseries
+        self.damping = self.nu * np.abs(self.sphere._laplacian_eigenvalues) ** self.diffusion_order
+                
+        self.forcing = forcing.Forcing(sphere, Si=Si)
             
-    def integrate_dynamics(self, temp_linear=True, vort_linear=True):
+    def integrate_dynamics(self):
         """
         Integrating function using leapfrog. By default, only the linear terms are considered for integration
         """
@@ -114,14 +107,11 @@ class Solver:
             u,v = self.sphere.vrtdiv2uv(zs[:, jnow], ds, realm='spec', grid='quad') #divergenceless flow
 
             # Compute advection tendencies
-            if vort_linear: #remove nonlinear terms
-                #Linear contributions fields:
+            if self.vort_linear: #Linear contributions fields:
                 du =  (z * self.sphere.V + (self.sphere.f + self.sphere.Z) * (self.sphere.V + v))
                 dv = -(z * self.sphere.U + (self.sphere.f + self.sphere.Z) * (self.sphere.U + u))
-                #du = self.sphere.f * v
-                #dv = -self.sphere.f * u
-            else:
-                #total fields:
+
+            else: #total fields:
                 du =  (self.sphere.f + self.sphere.Z + z)*(v+self.sphere.V)
                 dv = -(self.sphere.f + self.sphere.Z + z)*(u+self.sphere.U)
 
@@ -131,7 +121,8 @@ class Solver:
 
             # Step 3: Compute the curl of du, dv to find dzdt in spectral
             dz[:, inow], _ = self.sphere.sq.getvrtdivspec(du, dv, self.sphere._ntrunc)
-            dz[:, inow] += self.sphere.to_spectral(self.forcing[j]) #finally, add forcing term in spectral space
+            
+            dz[:, inow] += self.forcing.evolve_rededdy(dt=self.dt)#finally, add forcing term in spectral space
 
             # Step 1a: Compute tracer & gradients in grid space
             tr = self.sphere.to_quad_grid(trs[:, jnow])
@@ -140,7 +131,7 @@ class Solver:
             # Compute advection tendencies
             dtr = -u * self.sphere.dxthetam - self.sphere.U * dx_tr \
                   -v * self.sphere.dythetam - self.sphere.V * dy_tr
-            if not temp_linear:
+            if not self.temp_linear: #include if non-linear
                 dtr -= u * dx_tr + v * dy_tr
 
             # Add thermal relaxation to underlying temp field
@@ -151,25 +142,22 @@ class Solver:
             if j==0: #for dt difference in first step
                 #Step 4: Compute & apply damping 
                 #compute:  Z -> (Z-nu*L**n*zold)/(1+nu*2dt*L**n)
-                cV = 1. / (1. + self.Vdamping * self.dt)
-                cT = 1. / (1. + self.Tdamping * self.dt)
-
+                c = 1. / (1. + self.damping * self.dt)
 
                 # Apply damping to tendency
-                dz  [:, inow] = cV * (dz  [:,inow] - self.Vdamping * zs [:,jold])
-                dtrs[:, inow] = cT * (dtrs[:,inow] - self.Tdamping * trs[:,jold])
+                dz  [:, inow] = c * (dz  [:,inow] - self.damping * zs [:,jold])
+                dtrs[:, inow] = c * (dtrs[:,inow] - self.damping * trs[:,jold])
 
                 #Step 5: step forward in time; use eularian forward
                 zs [:, jnew] = zs [:, jnow] + self.dt * dz  [:,inow]
                 trs[:, jnew] = trs[:, jnow] + self.dt * dtrs[:,inow]
             else:
                 #Step 4: Compute & apply damping 
-                cV = 1. / (1. + self.Vdamping * 2 * self.dt)
-                cT = 1. / (1. + self.Tdamping * 2 * self.dt)
+                c = 1. / (1. + self.damping * 2 * self.dt)
 
                 # Apply damping to z tendency
-                dz  [:, inow] = cV * (dz  [:,inow] - self.Vdamping * zs [:,jold])
-                dtrs[:, inow] = cT * (dtrs[:,inow] - self.Tdamping * trs[:,jold])
+                dz  [:, inow] = c * (dz  [:,inow] - self.damping * zs [:,jold])
+                dtrs[:, inow] = c * (dtrs[:,inow] - self.damping * trs[:,jold])
                 
                 #Step 5: step forward in time; use leapfrog
                 zs [:, jnew] = zs [:, jold] + 2 * self.dt * dz  [:,inow]
@@ -207,8 +195,6 @@ class Solver:
 
         for i in range(N):
             uo[i],vo[i] = self.sphere.vrtdiv2uv(vort[i].values + self.sphere.Z_lin, self.sphere.vortp_div_lin, realm = 'grid', grid='linear')
-            #uo[i] = self.sphere.U + uo[i]
-            #vo[i] = vo
 
 
         crds = [vort.time[:], vort.y[:], vort.x[:]]
@@ -223,8 +209,3 @@ class Solver:
         v   = xr.DataArray(vo, name = 'v',   coords = crds, dims = ['time', 'y', 'x'])
         return xr.Dataset(data_vars = dict(vort=vort, vortp=vortp, u=u, v=v, thetap=thetap, theta=theta))
     
-    
-    def solve_diffusion_spectral(self, field_spectral, coeff, dt, order=1):
-        """:py:meth:`solve_diffusion` with spectral in- and output fields."""
-        eigenvalues_op = self.sphere._laplacian_eigenvalues ** order
-        return field_spectral / (1. + dt * coeff * eigenvalues_op)
