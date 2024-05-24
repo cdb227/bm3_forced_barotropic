8f0778d143a14c41387a37a728ef4bf7cb5e97c0
Cameron Bertossa <bertossa@rayleigh.aos.wisc.edu>
2024-05-24 11:46:33 -0500

    made update_forcing() flexible so it works with a variety of forcings, removed redundancies in **kwarg defaults across scripts

--- src/model/forcing.py
+++ src/model/forcing.py
@@ -7,7 +7,7 @@ from utils import config, constants
 
 class Forcing:
     """Class with some forcing scenarios for barotropic model."""
-    def __init__(self, sphere, Si=None):
+    def __init__(self, sphere, **kwargs):
         """
         Initializes forcing object for barotropic sphere.
 
@@ -15,20 +15,44 @@ class Forcing:
         sphere (Sphere): The sphere object representing the model domain.
         Si (ndarray, optional): Initial state for a stochastic process.
         """
+        
         self.sphere = sphere
-        self.Si = Si
+        
+        #forcing type
+        self.forcing_type = kwargs.get('forcing_type', config.DEFAULT_forcing_type)
+        self.A =            kwargs.get('forcing_A', config.DEFAULT_forcing_A) #forcing amplitude
+        
+        #for rededdy
+        self.Si  = kwargs.get('red_eddy_start', self.gen_Si())
+                                    
+        #for gaussian blob
+        self.blob_center = kwargs.get('blob_center', [60, 160])
+
+    def gen_Si(self):
+        Ai = np.random.normal(0, 1, size=(self.sphere.nspecindx))
+        Bi = np.random.normal(0, 1, size=(self.sphere.nspecindx))
+        return Ai + 1j * Bi 
+            
+    def evolve_forcing(self):
+        
+        if self.forcing_type=='zero_forcing':
+            self.force_term = self.zero_forcing()
+        elif self.forcing_type=='gaussian_blob':
+            self.force_term = self.gaussian_blob()
+        elif self.forcing_type=='rededdy':
+            self.force_term = self.evolve_rededdy()
+        else:
+            raise ValueError('forcing type not recognized')
+            
+        return self.force_term
 
     def zero_forcing(self):
         """Zero forcing case."""
         return 0.0
 
-    def evolve_rededdy(self, dt=None, A=1e-11):
+    def evolve_rededdy(self):
         """
         Evolves red eddy forcing represented by an Ornstein-Uhlenbeck process.
-
-        Parameters:
-        dt (float, optional): Timestep, required if `Si` is not None.
-        A (float, optional): Amplitude of the forcing. Default is 1e-11.
 
         Returns:
         ndarray: The evolved red eddy forcing in spectral space.
@@ -37,23 +61,14 @@ class Forcing:
         stir_width = 10.0  # degrees
         lat_mask = np.exp(-((np.abs(self.sphere.glats) - stir_lat) / stir_width) ** 2)  # eddy stirring location
         decorr_timescale = 7 * constants.day2sec  # 7 days
-        #decorr_timescale = 1e-10
 
         stirwn = np.where((8 <= self.sphere.specindxn) & (self.sphere.specindxn <= 12), 1, 0)  # force over a set of wavenumbers
 
-        Ai = np.random.normal(0, 1, size=(self.sphere.nspecindx))
-        Bi = np.random.normal(0, 1, size=(self.sphere.nspecindx))
+        self.Si = self.gen_Si() * np.sqrt(1 - np.exp(-2 * self.dt / decorr_timescale)) + np.exp(-self.dt / decorr_timescale) * self.Si
 
-        if self.Si is None:  # starting condition
-            self.Si = Ai + 1j * Bi
-        else:  # evolve condition
-            if dt is None:
-                raise ValueError("'dt' must be provided if initial 'Si' state is given.")
-            self.Si = (Ai + 1j * Bi) * np.sqrt(1 - np.exp(-2 * dt / decorr_timescale)) + np.exp(-dt / decorr_timescale) * self.Si
+        return self.A * self.sphere.to_spectral(self.sphere.to_linear_grid(self.Si * stirwn) * lat_mask)
 
-        return A * self.sphere.to_spectral(self.sphere.to_linear_grid(self.Si * stirwn) * lat_mask)
-
-    def gaussianblob(self, forcing_loc=[50, 160], A=10e-10):
+    def gaussian_blob(self):
         """
         Gaussian blob forcing timeseries representative of orography or other localized forcing.
 
@@ -72,9 +87,9 @@ class Forcing:
 
         def find_nearest_indx(array, value): return np.abs(array - value).argmin()
 
-        lat_i = find_nearest_indx(self.sphere.glat, forcing_loc[0])
-        lon_i = find_nearest_indx(self.sphere.glon, forcing_loc[1])
+        lat_i = find_nearest_indx(self.sphere.glat, self.blob_center[0])
+        lon_i = find_nearest_indx(self.sphere.glon, self.blob_center[1])
 
-        gauss_forcing[lat_i:lat_i + 10, lon_i:lon_i + 10] = g * A
+        gauss_forcing[lat_i:lat_i + 10, lon_i:lon_i + 10] = g * self.A
 
         return self.sphere.to_spectral(gauss_forcing)
